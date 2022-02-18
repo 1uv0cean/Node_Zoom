4e9d88dc31d30489b0b2454bea177a65baf5e200
songhwee1 <songhwee1@naver.com>
2022-02-19 01:16:19 +0900

    Connect socket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http"
+import WebSocket from "ws";
 import express from "express"
 
 const app = express();
@@ -5,7 +7,21 @@ const app = express();
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname + "/public"))
-app.get("/",(req,res) => res.render("home"))
+app.get("/",(_,res) => res.render("home"))
+app.get("/*",(_,res) => res.redirect("/"))
 
 const handleListen = () => console.log(`Listning on http://localhost:3000`)
-app.listen(3000, handleListen)
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (socket) =>{
+    console.log("Connected to Browser✅")
+    socket.on("close", () => console.log("Disconnected from the Browser❌"))
+    socket.on("message", (message) =>{
+        console.log(message.toString('utf-8'))
+    })
+    socket.send("Hello!")
+})
+
+server.listen(3000, handleListen)
