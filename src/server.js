97d0e3a574c5cd8be1b83bc95c1a7525a1ea30e7
songhwee1 <songhwee1@naver.com>
2022-03-07 23:18:31 +0900

    make room

--- src/server.js
+++ src/server.js
@@ -17,7 +17,13 @@ const httpServer = http.createServer(app)
 const wsServer = SocketIO(httpServer)
 
 wsServer.on("connection", socket => {
-    console.log(socket)
+    socket.onAny((event) =>{
+        console.log(`Socket Event: ${event}`)
+    })
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName)
+        done()
+    })
 })
 
 /*
