6e0bddaf5565944988d122b0ad2fed4c2d9bb155
songhwee1 <songhwee1@naver.com>
2022-04-24 13:57:33 +0900

    change to non-face-to-face clinic platform

--- src/server.js
+++ src/server.js
@@ -23,6 +23,10 @@ wsServer.on("connection", socket => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName)
         done()
+        socket.to(roomName).emit("welcome")
+    })
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"))
     })
 })
 
